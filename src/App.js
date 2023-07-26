454ca2fd1ac06be8d108dfb2cc6fb4b2e4d605d8
deboradeotti <debora.deotti@gmail.com>
2023-07-26 19:15:49 -0300

    adds features to complete the game

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
 import React from "react";
 import Die from "./Die";
 import { nanoid } from 'nanoid';
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti';
 
 export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+   const allDiceHeld = dice.every(die => die.isHeld);
+   const allSameValue = dice.every(die => die.value === dice[0].value)
+   
+   if (allDiceHeld && allSameValue) {
+    setTenzies(true);
+   }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      const num = Math.floor(Math.random() * 6) + 1;
-      const die = {
-        value: num,
-        isHeld: true,
-        id: nanoid()
-      }
-      newDice.push(die);
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
   const dieElements = dice.map(die => (
-    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
   ))
 
   function rollDice() {
-    setDice(allNewDice());
+    if(tenzies) {
+      setTenzies(false);
+      setDice(allNewDice);
+    } else {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : generateNewDie()
+      }))
+    }
   }
+
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return id === die.id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  const { width, height } = useWindowSize()
 
   return(
     <main className="board">
-      <h1 className="board__title">Tenzies</h1>
-      <p className="board__description">Roll until all dice are the same. Click<br></br>each dice to freeze it at its current value<br></br>between rolls.</p>
+      {tenzies && <Confetti width={width} height={height} />}
+      <div>
+        <h1 className="board__title">Tenzies</h1>
+        <p className="board__description">Roll until all dice are the same. Click<br></br>each die to freeze it at its current value<br></br>between rolls.</p>
+      </div>
       <div className="dice-container">
         {dieElements}
       </div>
-      <button className="button" onClick={rollDice}>Roll</button>
+      <button className="button" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
--- src/Die.js
+++ src/Die.js
@@ -1,6 +1,6 @@
 export default function Die(props) {
   return(
-    <div className={`die ${props.isHeld && "held"}`}>
+    <div className={`die ${props.isHeld ? "held" : ""}`} onClick={props.holdDice}>
       <h2 className="die__number">{props.value}</h2>
     </div>
   )
